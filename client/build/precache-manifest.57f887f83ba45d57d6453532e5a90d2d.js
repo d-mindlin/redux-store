7bbdda7939dcf88a7b2895e48e9e59d3ba5cc311
Daniel Mindlin <daniel-mindlin@Daniels-MacBook-Pro.local>
2020-08-19 12:27:20 -0700

    postbuild

---
+++ client/build/precache-manifest.57f887f83ba45d57d6453532e5a90d2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e349895394b420e2ae1bc14f35750272",
+    "url": "/index.html"
+  },
+  {
+    "revision": "185423ec28eec5852a6b",
+    "url": "/static/css/main.49b21ed8.chunk.css"
+  },
+  {
+    "revision": "16f4b1e124ecda48c1fa",
+    "url": "/static/js/2.8db5df9a.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.8db5df9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "185423ec28eec5852a6b",
+    "url": "/static/js/main.90d0488a.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
